f2d33195f15e97f9218f50403167b3d04a2ca118
mbesancon <mathieu.besancon@gmail.com>
2018-10-19 15:19:26 +0200

    added string representation

--- src/lib.rs
+++ src/lib.rs
@@ -247,3 +247,66 @@ impl Game {
         self.map.elements.push(Element{pos : p, kind : ek, time_left : tl});
     }
 }
+
+/// Builds representation of various objects
+/// but more specifically of the Game struct
+pub mod str_rep {
+    use std::string::ToString;
+    use super::*;
+
+    type Point = (Position, char);
+
+    trait Represent {
+        fn represent(&self) -> Vec<Point>;
+    }
+
+    impl Represent for Map {
+        fn represent(&self) -> Vec<Point> {
+            self.elements.iter().map(|el|
+                match el.kind {
+                    ElementKind::Apple => (el.pos, '@'),
+                    ElementKind::Bush  => (el.pos, '+'),
+                    ElementKind::Rock  => (el.pos, '#'),
+                    _ => (el.pos, '.'),
+                }
+            ).collect()
+        }
+    }
+
+    impl Represent for Snake {
+        fn represent(&self) -> Vec<Point> {
+            let head = match self.direction {
+                Direction::Down => 'v',
+                Direction::Up   => '∧',
+                Direction::Left => '>',
+                Direction::Right => '<',
+            };
+            let mut v = vec![(*self.positions.get(0).unwrap(), head)];
+            let n = self.positions.len();
+            for idx in 1..n {
+                v.push(((*self.positions.get(idx).unwrap()), '■'));
+            }
+            v
+        }        
+    }
+        
+    impl ToString for Game {
+        fn to_string(&self) -> String {
+            let mut s = (0..self.map.height)
+                .map(|_|
+                    (0..self.map.width).map(|_| ' '
+                    ).collect::<Vec<_>>()
+                )
+                .collect::<Vec<_>>();
+            let map = self.map.represent();
+            let snake = self.snake.represent();
+            for (p, c) in map.iter() {
+                s[p.y as usize][p.x as usize] = *c;
+            }
+            for (p, c) in snake.iter() {
+                s[p.y as usize][p.x as usize] = *c;
+            }
+            s.join(&'\n').iter().collect()
+        }
+    }
+}
