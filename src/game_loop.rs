7273cbbaa1a54fba3525e66b0cf249830b1c046f
mbesancon <mathieu.besancon@gmail.com>
2018-10-21 20:18:17 +0200

    game window and dispatch

--- src/game_loop.rs
+++ src/game_loop.rs
@@ -7,11 +7,30 @@ const GREY: types::Color = [0.9, 0.9, 0.9, 0.9];
 
 /// Main App with graphic engine and game state
 pub struct App {
-    gl: GlGraphics, // OpenGL drawing backend.
-    game: Game,     // Game state
+    gl: GlGraphics,       // OpenGL drawing backend.
+    game: Game,           // Game state
+    window: GlutinWindow, // window manager
 }
 
 impl App {
+    /// Game event loop rendering, changing the game state and dispatching on key strokes
+    pub fn event_loop(&mut self, events: &mut Events) {
+        while let Some(e) = events.next(&mut self.window) {
+            if let Some(r) = e.render_args() {
+                self.render(&r);
+            }
+            if let Some(u) = e.update_args() {
+                self.update(&u);
+            }
+            if let Some(p) = e.press_args() {
+                let terminate = !self.key_dispatch(p);
+                if terminate {
+                    return;
+                }
+            }
+        }
+    }
+
     pub fn render(&mut self, args: &RenderArgs) {
         let square = rectangle::square(0.0, 0.0, 50.0);
         let (x, y) = ((args.width / 2) as f64, (args.height / 2) as f64);
@@ -23,5 +42,18 @@ impl App {
     }
     pub fn update(&mut self, args: &UpdateArgs) {
         self.game.next(args.dt);
+    }
+
+    /// handles keyboard action, returns whether the game continues
+    pub fn key_dispatch(&mut self, input: Button) -> bool {
+        match input {
+            Button::Keyboard(Key::Down) => self.game.change_dir(Direction::Down),
+            Button::Keyboard(Key::Up) => self.game.change_dir(Direction::Up),
+            Button::Keyboard(Key::Left) => self.game.change_dir(Direction::Left),
+            Button::Keyboard(Key::Right) => self.game.change_dir(Direction::Right),
+            Button::Keyboard(Key::Escape) => return false,
+            _ => (),
+        };
+        return true;
     }
 }
